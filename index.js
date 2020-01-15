e8fb815c72ec5fd6a7a7390b8aefc17f70cf6820
Xu Xing <xing.xu@intel.com>
2020-01-15 15:35:35 +0800

    Add tfjs application template

---
+++ index.js
@@ -0,0 +1,45 @@
+/**
+ * @license
+ * Copyright 2019 Google LLC. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================================
+ */
+
+//import * as handtrack from '@tensorflow-models/handtrack';
+//import * as tfwebgpu from '@tensorflow/tfjs-backend-webgpu';
+import * as tf from '@tensorflow/tfjs-core';
+
+
+tf.ENV.set('DEBUG',true);
+
+/**
+ * Start the demo.
+ */
+const bindPage = async () => {
+  //await tf.ready();
+  // f(a, b) = a * b
+  const f = (a, b) => a.mul(b);
+  // df / da = b, df / db = a
+  const g = tf.grads(f);
+
+  const a = tf.tensor1d([2, 3]);
+  const b = tf.tensor1d([-2, -3]);
+  const [da, db] = g([a, b]);
+  console.log('da');
+  da.print();
+  console.log('db');
+  db.print();
+
+}
+
+bindPage();
